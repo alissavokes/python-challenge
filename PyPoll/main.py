8a0eab508daa1b595d5f009720453368d74de7aa
alissavokes <alissavokes@gmail.com>
2020-06-21 19:26:36 -0400

    formatting fixed for percent votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -28,7 +28,20 @@ with open(csvpath) as election_data_csv:
     print(f"Total Votes: {total_votes}")
     print("-------------------------")
 
+    prior_votes = 0
     for candidate in candidate_dict:
         votes = len(candidate_dict[candidate])
-        percentage_votes = "{:.3%}".format(votes / total_votes * 100)
-        print(f"{candidate}: {percentage_votes} ({votes})")
+        percentage_votes = "{:.3f}".format(votes / total_votes * 100)
+        print(f"{candidate}: {percentage_votes}% ({votes})")
+        if votes > prior_votes:
+            election_winner = candidate
+        prior_votes = votes
+    print("-------------------------")
+    print(f"Winner: {election_winner}")
+
+#output_file = os.path.join("Analysis", "election_results.txt")
+#with open(output_file, "w") as datafile:
+    #datafile.write("Election Results\n")
+    #datafile.write("-------------------------\n")
+    #datafile.write(f"Total Votes: {total_votes}\n")
+    #datafile.write("-------------------------\n")
